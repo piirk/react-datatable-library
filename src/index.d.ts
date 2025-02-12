a6a95b00671547357ed8698798678315400c2c26
piirk <baptiste.boucharechas@gmail.com>
2025-02-12 09:13:15 +0100

    Add optional useMUI prop to DataTableProps interface

--- src/index.d.ts
+++ src/index.d.ts
@@ -8,6 +8,7 @@ declare module 'react-datatable-library' {
   export interface DataTableProps {
     data: any[]
     columns: DataTableColumn[]
+    useMUI?: boolean
   }
 
   export const DataTable: React.FC<DataTableProps>

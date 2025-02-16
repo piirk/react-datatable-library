c304358bc7c81613c4973ff32ff1dc7dfa8b2caa
piirk <baptiste.boucharechas@gmail.com>
2025-02-16 17:20:58 +0100

    Refactor type declaration for DataTable component in index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,3 @@
 import { DataTableProps } from './types'
 
-declare module 'react-datatable-library' {
-  export const DataTable: React.FC<DataTableProps>
-}
+export const DataTable: React.FC<DataTableProps>
